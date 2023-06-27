67df0367a57d1a105c1ba074ee634c05dae8e254
killer <michael.molchanov.2004@gmail.com>
2023-06-27 09:14:40 +0200

    i have no idea how to do it but i added . in the end of docs

--- playground/Common/IMaze.cs
+++ playground/Common/IMaze.cs
@@ -6,17 +6,17 @@ namespace Common;
 public interface IMaze
 {
     /// <summary>
-    /// Gets the size of maze on X axis. Returns number of blocks(groups of cells)
+    /// Gets the size of maze on X axis. Returns number of blocks(groups of cells).
     /// </summary>
     int MazeSizeX { get; }
 
     /// <summary>
-    /// Gets the size of maze on Y axis. Returns number of blocks(groups of cells)
+    /// Gets the size of maze on Y axis. Returns number of blocks(groups of cells).
     /// </summary>
     int MazeSizeY { get; }
 
     /// <summary>
-    /// Gets the block size. Returns number of cells in block side(block is a square, block size is it's side in cells)
+    /// Gets the block size. Returns number of cells in block side(block is a square, block size is it's side in cells).
     /// </summary>
     int BlockSize { get; }
 
