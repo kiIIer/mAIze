935fdb57269fc634bc578da4d5048b4443fc7904
killer <michael.molchanov.2004@gmail.com>
2023-06-27 09:12:05 +0200

    basic IMaze properties

--- playground/Common/IMaze.cs
+++ playground/Common/IMaze.cs
@@ -5,4 +5,20 @@ namespace Common;
 /// </summary>
 public interface IMaze
 {
+    /// <summary>
+    /// Gets the size of maze on X axis. Returns number of blocks(groups of cells)
+    /// </summary>
+    int MazeSizeX { get; }
+    
+    /// <summary>
+    /// Gets the size of maze on Y axis. Returns number of blocks(groups of cells)
+    /// </summary>
+    int MazeSizeY { get; }
+
+    /// <summary>
+    /// Gets the block size. Returns number of cells in block side(block is a square, block size is it's side in cells)
+    /// </summary>
+    int BlockSize { get; }
+    
+    
 }
